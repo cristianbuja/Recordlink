fa5a195cea5d1bb7e1652fb706b6afcdb1b87b2e
Cristian Buja <cristian.buja@intera.it>
2017-04-09 01:02:22 +0200

    New changelist

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,29 +11,29 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Record Link',
-	'description' => 'Add custom link to records like system category or news',
-	'category' => 'plugin',
-	'version' => '1.2.0',
-	'state' => 'beta',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearcacheonload' => false,
-	'author' => 'Cristian Buja',
-	'author_email' => 'cristian.buja@intera.it',
-	'author_company' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '7.6.0-8.7.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
+  'title' => 'Record Link',
+  'description' => 'Add custom link to records like system category or news',
+  'category' => 'plugin',
+  'version' => '1.3.0',
+  'state' => 'beta',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearcacheonload' => false,
+  'author' => 'Cristian Buja',
+  'author_email' => 'cristian.buja@intera.it',
+  'author_company' => NULL,
+  'constraints' =>
+  array (
+    'depends' =>
+    array (
+      'typo3' => '7.6.0-8.7.99',
+    ),
+    'conflicts' =>
+    array (
+    ),
+    'suggests' =>
+    array (
+    ),
+  ),
 );
 
